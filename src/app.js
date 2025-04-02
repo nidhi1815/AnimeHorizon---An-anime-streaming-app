c029023d00fccb9444df3fb4c9313961361fd16a
Nidhi Kumari <nidhikumari_22700@aitpune.edu.in>
2025-04-03 01:15:18 +0530

    postman

--- src/app.js
+++ src/app.js
@@ -2,14 +2,28 @@ import express, { urlencoded } from "express"
 
 import cookieParser from "cookie-parser"
 import cors from "cors"
+//routes import
+import router from "./routes/user.routes.js"
 const app = express()
 
 
 app.use( cors({
     origin: process.env.CORS_ORIGIN
 }))
+
 app.use( express.json({limit : "16kb"}))
 app.use( express.urlencoded({extended: true}) )
 app.use(express.static("public"))
-app.use(cookieParser)
+app.use(cookieParser())
+
+
+
+
+
+//routes declaration
+app.use("/api/v1/user" ,  router)
+// app.use("/api/v1/", router)
+
+
+
 export { app }
